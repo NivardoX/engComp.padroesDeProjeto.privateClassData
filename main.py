00e81d085408a697b69c474af207f7b8ccd53780
Nivardo Albuquerque <>
2021-06-07 18:43:31 +0000

    Add replit support

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ class DataClass:
     """
 
     def __init__(self):
+        print("Criando dataclass")
         self.value = None
 
     def __get__(self, instance, owner):
+        print("Pegando o valor de dentro da dataclass")
         return self.value
 
     def __set__(self, instance, value):
         if self.value is None:
+            print("Settando o valor a dentro da dataclass")
             self.value = value
 
 
@@ -22,13 +25,15 @@ class MainClass:
     attribute = DataClass()
 
     def __init__(self, value):
+        print("Criando MainClass")
+
         self.attribute = value
 
 
 def main():
     m = MainClass(True)
     m.attribute = False
-
+    print(m.attribute)
 
 if __name__ == "__main__":
     main()
